1e22dc2238b7b1a643c833feec6572354d3b5f34
Jeevan Chavan <jeevanchavan7273@gmail.com>
2025-12-05 14:18:28 +0530

    Add new task drop and count value change

--- app.js
+++ app.js
@@ -2,35 +2,91 @@ const todo = document.querySelector('#todo');
 const progress = document.querySelector('#progress');
 const done = document.querySelector('#done');
 
-// console.log(todo,progress,done);
+let dargElement = null;
+let columns = [todo,progress,done];
 
 const tasks = document.querySelectorAll('.task');
 
 tasks.forEach((task)=>{
     task.addEventListener("drag",(e)=>{
         // console.log(e)
+        dargElement = task;
     })
 })
-
-// progress.addEventListener('dragenter',(e)=>{
-//     progress.classList.add('hover-over');
-// })
-
-// progress.addEventListener('dragleave',(e)=>{
-//     progress.classList.remove('hover-over');
-// })
 
 function AddDragEventsOnColumn(column){
     column.addEventListener('dragenter',(e)=>{
         e.preventDefault();
         column.classList.add('hover-over')
     })
+
     column.addEventListener('dragleave',(e)=>{
         e.preventDefault();
         column.classList.remove('hover-over')
     })
+
+    column.addEventListener('dragover',(e)=>{
+        e.preventDefault();
+    })
+
+    column.addEventListener('drop',(e)=>{
+        e.preventDefault();
+
+        column.appendChild(dargElement);
+        column.classList.remove('hover-over');
+
+        columns.forEach(col =>{
+            let tasks = col.querySelectorAll('.task')
+            let count = col.querySelector('.right')
+
+            count.innerText = tasks.length;
+        })
+    })
+
+    
 }
 
 AddDragEventsOnColumn(todo)
 AddDragEventsOnColumn(progress)
 AddDragEventsOnColumn(done)
+
+const toggleModalBtn = document.querySelector('#toggle-modal');
+const modal = document.querySelector('.modal');
+const bg = document.querySelector('.modal .bg');
+const addTaskBtn = document.querySelector("#add-new-task")
+
+toggleModalBtn.addEventListener('click',()=>{
+    modal.classList.toggle('active')
+})
+
+bg.addEventListener('click',()=>{
+    modal.classList.remove('active')
+})
+
+addTaskBtn.addEventListener('click',()=>{
+    let titleInput = document.querySelector("#task-title-input").value;
+    let descInput = document.querySelector("#task-desc-input").value;
+
+    let div = document.createElement('div');
+    div.classList.add("task")
+
+    div.setAttribute("draggable","true");
+    div.innerHTML = `<h2>${titleInput}</h2>
+                    <p>${descInput}</p>
+                    <button>Delete</button>`
+
+    todo.appendChild(div);
+
+    columns.forEach(col =>{
+        let tasks = col.querySelectorAll('.task')
+        let count = col.querySelector('.right')
+
+        count.innerText = tasks.length;
+    })
+
+    div.addEventListener("drag",()=>{
+        dargElement = div;
+    })
+
+    modal.classList.remove('active');
+})
